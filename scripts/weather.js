c5e21bab21ded224b4060a5850a6db371ef1c35d
Vera <17062292@student.hhs.nl>
2019-03-19 12:26:51 +0100

    Update weather.js

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,1 +1,1 @@
-http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={3e55ddc5955b7f88c5f00cfff878c77c}
+api.openweathermap.org/data/2.5/forecast?id=524901&APPID={3e55ddc5955b7f88c5f00cfff878c77c}
