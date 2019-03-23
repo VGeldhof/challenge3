9a515a335f94340b2d05dd62ce647868b74ac782
Vera <17062292@student.hhs.nl>
2019-03-23 12:14:48 +0100

    updated

--- scripts/weather.js
+++ scripts/weather.js
@@ -69,7 +69,7 @@ function weatherIcon() {
 	var bodyIcon = document.getElementById('icon');
 	var hour = today.getHours();
 
-	if (response.weather[0].description = "clear sky" && hour < 12) {
+	if (response.weather[0].description = 'clear sky' && hour < 12) {
 		bodyIcon.style.backgroundImage = 'http://openweathermap.org/img/w/01d.png';
 	}
 	else {
@@ -102,7 +102,8 @@ function getAPIdataZomato() {
 		headers: {
 			'user-key': '79c55a16192a56bb6f9e4dec7af22bc5'
 		}
-})
+	
+	})
 	
 	// parse to JSON format
 	.then(function(response) {
@@ -133,19 +134,84 @@ function onAPISuccesZomato(response) {
 
 	var type = response.location_suggestions[0].name;
 
-	// var restaurant = response.location_suggestions[0].cuisines.cuisine.cuisine_name;
-
 	var foodType = document.getElementById('cityName');
-
-	// var restaurantCuisine = document.getElementById('listRestaurants');
 	
 	foodType.innerHTML = type;
-
-	// restaurantCuisine.innerHTML = restaurant;
 
 }
 
 
+
+
+function getAPIdataZomatoCollections() {
+
+	var url = "https://developers.zomato.com/api/v2.1/collections";
+	var apiKey ="79c55a16192a56bb6f9e4dec7af22bc5";
+	var city = "277";
+
+	// construct request
+	var request = url + "?city_id=" + city;
+
+	var myHeaders = new Headers();
+
+	myHeaders.append('user-key', '79c55a16192a56bb6f9e4dec7af22bc5');	
+	
+
+	// get current city details
+	fetch(request, {
+		method:'GET', 
+		headers: {
+			'user-key': '79c55a16192a56bb6f9e4dec7af22bc5'
+		}
+	})
+	
+	// parse to JSON format
+	.then(function(response) {
+		// if niet ok dan is er iets mis gegaan.
+		if(!response.ok) {
+			// throw betekent gooien. Dit wordt naar catch gegooid.
+			throw Error(response.statusText);
+		}
+		return response.json();
+	})
+	
+	// render weather per day
+	// Als er geen error is wordt dit gedaan en voert die de functie onAPISucces uit.
+	.then(function(response) {
+		// render weatherCondition
+		onAPISuccesZomatoCollections(response);	
+	})
+	
+	// catch error
+	// Hier vang je de fout op.
+	// als je de fout gevangen hebt moet je dit gaan uitvoeren.
+	.catch(function (error) {
+		onAPIError(error);
+	});
+}
+
+function onAPISuccesZomatoCollections(response) {
+	console.log(response)
+
+	var restaurant = response.collections[0].collection.title;
+
+	var collectionTitle = document.getElementById('collectionTitle');
+
+	collectionTitle.innerHTML = restaurant;
+
+	var restaurantDescription = response.collections[0].collection.description;
+
+	var collectionDescription = document.getElementById('collectionDescription');
+
+	collectionDescription.innerHTML = restaurantDescription;
+
+
+
+	var restaurantURL = response.collections[0].collection.url;
+	var collectionURL = document.getElementById('collectionIMG').setAttribute('src', response.collections[0].collection.image_url);
+
+
+}
 
 
 
@@ -173,6 +239,8 @@ function onAPIError(error) {
 getAPIdata();
 
 getAPIdataZomato();
+
+getAPIdataZomatoCollections();
 
 
 
